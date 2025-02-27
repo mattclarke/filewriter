588d5d125ac1128aac062b6423f73f5ec4f22c65
Matt Clarke <matto.kuraku@gmail.com>
2025-02-27 17:12:29 +0100

    wip

--- src/ev44.rs
+++ src/ev44.rs
@@ -1,18 +1,38 @@
 extern crate flatbuffers;
+use flatbuffers::Vector;
+
 use crate::ev44_events_generated::root_as_event_44_message;
 
-struct Ev44 {}
+struct Ev44<'a> {
+    source: String,
+    message_id: i64,
+    reference_time: Vector<'a, i64>,
+}
 
-impl Ev44 {
+impl Ev44<'_> {
     fn get_source(buf: &[u8]) -> String {
         let root = root_as_event_44_message(buf).unwrap();
         root.source_name().to_string()
+    }
+
+    fn new(buf: &[u8]) -> Ev44 {
+        let source = Ev44::get_source(&buf);
+        let root = root_as_event_44_message(buf).unwrap();
+        let message_id = root.message_id();
+        let reference_time = root.reference_time();
+        dbg!(reference_time);
+
+        Ev44 {
+            source,
+            message_id,
+            reference_time,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::ev44_events_generated::{root_as_event_44_message, Event44Message, Event44MessageArgs};
+    use crate::ev44_events_generated::{Event44Message, Event44MessageArgs};
 
     use super::*;
 
@@ -47,5 +67,16 @@ mod tests {
         let buf = create_flatbuffer();
 
         assert_eq!(Ev44::get_source(&buf.as_slice()), "SomeSource");
+    }
+
+    #[test]
+    fn extract_ev44() {
+        let buf = create_flatbuffer();
+
+        let ev44 = Ev44::new(&buf.as_slice());
+
+        assert_eq!(ev44.source, "SomeSource");
+        assert_eq!(ev44.message_id, 123);
+        //assert_eq!(ev44.reference_time, vec![123456_i64]);
     }
 }
